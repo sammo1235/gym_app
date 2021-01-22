febd702f018a011427d8675cccc3ed6f8157c46e
sammo1235 <samrhysperry93@hotmail.co.uk>
2021-01-22 08:24:40 +0000

    test assert raises when lift not found

--- test/models/sett_test.rb
+++ test/models/sett_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 class SettTest < ActiveSupport::TestCase
-  test '#workload calculates volume of sett' do
+  setup do
     default_workout
+  end
 
+  test '#workload calculates volume of sett' do
     assert_equal 200, @workout.setts.first.workload
     assert_equal 150, @workout.setts.second.workload
   end
 
   test 'Sett.user_best_for_lift gives user PB set for a given lift' do
-    default_workout
-
     assert_equal 200, Sett.user_best_for_lift(@user, @squat)
 
     assert_difference 'Sett.user_best_for_lift(@user, @squat)', 20 do
@@ -19,14 +19,10 @@ class SettTest < ActiveSupport::TestCase
   end
 
   test 'Sett.user_best_for_lift also works with lift name only' do
-    default_workout
-
     assert_equal 200, Sett.user_best_for_lift(@user, 'Back Squat')
   end
 
   test 'Sett.user_best gives projected best 1RM for sets with 4-6 reps...' do
-    default_workout
-
     Workout.new(variant: 0, user: @user).tap do |workout|
       workout.setts << Sett.create(weight: 190, reps: 5, lift: @squat)
       workout.save
@@ -36,13 +32,17 @@ class SettTest < ActiveSupport::TestCase
   end
 
   test '...but other rep ranges dont affect it' do
-    default_workout
-
     Workout.new(variant: 0, user: @user).tap do |workout|
       workout.setts << Sett.create(weight: 160, reps: 10, lift: @squat)
       workout.save
     end
 
     assert_equal 200, Sett.user_best_for_lift(@user, @squat)
+  end
+
+  test "Sett.user_best raises error if lift doesn't exist" do
+    assert_raises ActiveRecord::RecordNotFound do
+      Sett.user_best_for_lift(@user, 'Chawomba')
+    end
   end
 end
