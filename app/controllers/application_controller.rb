cf858c1d09a632c62302056f06734e850ff8b6d8
sammo <samrhysperry93@hotmail.co.uk>
2021-02-18 08:46:54 +0000

    Add user avatar and style workouts index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :confirgure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def confirgure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :bodyweight, :gender])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :bodyweight, :gender, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :age, :bodyweight, :gender, :avatar])
   end
 end
